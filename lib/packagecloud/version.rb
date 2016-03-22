74a8ad8ca3a22c96b69eda0eecb095c6a9fff4ba
capotej <jcapote@gmail.com>
2016-03-22 11:31:16 -0400

    bump version

--- lib/packagecloud/version.rb
+++ lib/packagecloud/version.rb
@@ -1,7 +1,7 @@
 module Packagecloud
   MAJOR_VERSION = "1"
   MINOR_VERSION = "0"
-  PATCH_VERSION = "2"
+  PATCH_VERSION = "3"
 
   VERSION = [MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION].join(".")
 end
